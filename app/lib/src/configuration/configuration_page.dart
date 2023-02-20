0d7c92bbd7799c7657f2c93a9eef73df1cec2500
rmn.dev.mobility <rmn.dev.mobility@gmail.com>
2023-02-20 19:49:53 -0300

    Aula 6 - ValueNotifier

--- app/lib/src/configuration/configuration_page.dart
+++ app/lib/src/configuration/configuration_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../shared/store/app_store.dart';
 
 class ConfigurationPage extends StatefulWidget {
   const ConfigurationPage({super.key});
@@ -10,9 +13,64 @@ class ConfigurationPage extends StatefulWidget {
 class _ConfigurationPageState extends State<ConfigurationPage> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final appStore = context.watch<AppStore>(
+      (store) => store.themeMode,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('LISTINHA'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Text(
+                'Configurações',
+                style: textTheme.titleLarge,
+              ),
+            ),
+            Text(
+              'Tema',
+              style: textTheme.titleMedium,
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: appStore.themeMode.value,
+              title: const Text('Sistema'),
+              onChanged: appStore.changeThemeMode,
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: appStore.themeMode.value,
+              title: const Text('Claro'),
+              onChanged: appStore.changeThemeMode,
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: appStore.themeMode.value,
+              title: const Text('Escuro'),
+              onChanged: appStore.changeThemeMode,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Text(
+                'Controle de dados',
+                style: textTheme.titleMedium,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('Apagar cache e reiniciar o app')),
+            ),
+          ],
+        ),
       ),
     );
   }
